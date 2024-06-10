6b599cf253fe16ab2b00b8f3e93c58c014fc2833
한규호 <hkh7670@gmail.com>
2024-06-10 18:30:54 +0900

    data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO user_info (USER_ID, USER_NAME, IS_USED, REG_DATE, UPT_DATE)
 VALUES ('ghhan', '한규호', true, now(), now());
+
+INSERT INTO user_point (USER_SEQ, currency, balance, REG_DATE, UPT_DATE)
+VALUES (1, 'USD', 100.15, now(), now());
+
+INSERT INTO user_point (USER_SEQ, currency, balance, REG_DATE, UPT_DATE)
+VALUES (1, 'KRW', 100000, now(), now());
